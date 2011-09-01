74e0c029ffa57aff127204041d669a6fe8677837
pasberth <pasberth@gmail.com>
2011-09-02 00:38:58 +0900

    EndlessRuby

---
+++ src/endlessruby.rb
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+module Kernel
+
+  alias endlessruby_original_require require
+
+  def require path
+    endlessruby_original_require path
+  rescue Exception
+    begin
+      open(path) do |file|
+        temp = Object.new
+        temp.extend EndlessRuby
+        temp.instance_eval do
+          Kernel.eval endless_ruby_to_pure_ruby(file.read)
+        end
+      end
+    rescue => e
+      $@ = caller
+      raise e
+    end
+  end
+
+end
+
+module EndlessRuby
+
+  def blank_line? line
+    return true unless line
+    (line.chomp.gsub /\s+?/, '') == ""
+  end
+
+  def unindent line
+    line  =~ /^\s*?(\S.*?)$/
+    $1
+  end
+
+  def indent line, level, indent="  "
+    "#{indent * level}#{line}"
+  end
+
+  def indent_count line, indent="  "
+    return 0 unless line
+    if line =~ /^#{indent}(.*?)$/
+      1 + (indent_count $1, indent)
+    else
+      0
+    end
+  end
+
+  BLOCK_KEYWORDS = [
+    [/^if.*?$/, /^elsif.*?$/, /^else.*?$/],
+    [/^unless.*?$/],
+    [/^while.*?$/],
+    [/^until.*?$/],
+    [/^case.*?$/, /^when.*?$/, /^else.*?$/],
+    [/^def.*?$/],
+    [/^class.*?$/],
+    [/^.*?\s+do.*?$/]
+  ]
+
+  def endless_ruby_to_pure_ruby src
+    endless = src.split "\n"
+    endless.reject! { |line| blank_line? line }
+    pure = []
+    i = 0
+    while i < endless.length
+      pure += [(currently_line = endless[i])]
+      " ブロックを作らない構文なら単に無視する "
+      next i += 1 unless BLOCK_KEYWORDS.any? { |k| k[0] =~ unindent(currently_line)  }
+      keyword = BLOCK_KEYWORDS.each { |k| break k if k[0] =~ unindent(currently_line)  }
+      currently_indent_depth = indent_count currently_line
+      just_after_indent_depth = indent_count endless[i + 1]
+      case currently_indent_depth <=> just_after_indent_depth
+      when -1; "インデントが深いならブロックを始める"
+        base_indent_depth = currently_indent_depth
+        inner_statements = []
+        while i < endless.length
+          break unless keyword[1..-1].any? { |k| k =~ unindent(endless[i + 1]) } || base_indent_depth < indent_count(endless[i + 1])
+          inner_statements << endless[i + 1]
+          i += 1
+        end
+        pure += endless_ruby_to_pure_ruby(inner_statements.join("\n")).split "\n"
+        pure += ["#{'  '*currently_indent_depth}end"]
+      when 0; "インデントが同じなら単に無視する"
+      when 1; "インデントが浅いなら"
+      end
+      i += 1
+    end
+    pure.join "\n"
+  end
+
+end
---
+++ test/test.rb
@@ -0,0 +1,89 @@
+ROOT = File.dirname(File.dirname(File.expand_path(__FILE__)))
+
+require "#{ROOT}/src/endlessruby.rb"
+require "test/unit"
+class TestEndlessRuby < Test::Unit::TestCase
+  
+  include EndlessRuby
+  
+  def setup
+  end
+  
+  def test_blank_line
+    assert_equal true, (blank_line? "        ")
+  end
+
+  def test_not_blank_line
+    assert_equal false, (blank_line? "        pass")
+  end
+  
+  def test_1
+    input_src = <<DEFINE
+def test
+  test
+DEFINE
+    output_src = <<DEFINE
+def test
+  test
+end
+DEFINE
+    output_src.chomp!
+    assert_equal output_src, (endless_ruby_to_pure_ruby input_src)
+  end
+
+  def test_2
+    input_src = <<DEFINE
+class Test
+  def test
+    proc do |args|
+      pass
+    proc do |args|
+      pass
+DEFINE
+    output_src = <<DEFINE
+class Test
+  def test
+    proc do |args|
+      pass
+    end
+    proc do |args|
+      pass
+    end
+  end
+end
+DEFINE
+    output_src.chomp!
+    assert_equal output_src, (endless_ruby_to_pure_ruby input_src)
+  end
+
+  def test_3
+    input_src = <<DEFINE
+def test
+  if false
+    pass
+  elsif true
+    pass
+  else
+    pass
+DEFINE
+    output_src = <<DEFINE
+def test
+  if false
+    pass
+  elsif true
+    pass
+  else
+    pass
+  end
+end
+DEFINE
+    output_src.chomp!
+    assert_equal output_src, (endless_ruby_to_pure_ruby input_src)
+  end
+
+  def test_require
+    require "#{ROOT}/test/foo.er"
+    assert_equal "hello", Foo.hello
+  end
+
+end
