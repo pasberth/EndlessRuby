335898ba4154e25f8fa3d1771a8da3b8ca8e642c
pasberth <pasberth@gmail.com>
2011-11-16 23:14:14 +0900

    バグを再現するテストを追加

--- test/test_simply.rb
+++ test/test_simply.rb
@@ -113,4 +113,41 @@ DEFINE
     output_src.chomp!
     assert_equal output_src, (endless_ruby_to_pure_ruby input_src)
   end
+
+
+  def test_5
+    input_src = <<DEFINE
+def method
+  src = <<SRC
+hello world
+SRC
+DEFINE
+    output_src = <<DEFINE
+def method
+  src = <<SRC
+hello world
+SRC
+end
+DEFINE
+    output_src.chomp!
+    assert_equal output_src, (endless_ruby_to_pure_ruby input_src)
+  end
+
+  def test_5_2
+    input_src = <<DEFINE
+def method
+  src = <<-SRC
+hello world
+SRC
+DEFINE
+    output_src = <<DEFINE
+def method
+  src = <<-SRC
+hello world
+SRC
+end
+DEFINE
+    output_src.chomp!
+    assert_equal output_src, (endless_ruby_to_pure_ruby input_src)
+  end
 end
