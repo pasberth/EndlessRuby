bc7022b6f5e6a15d26ced1b73a9b0e7679da5c1e
pasberth <pasberth@gmail.com>
2011-11-23 14:27:46 +0900

    リファクタ

--- lib/endlessruby.rb
+++ lib/endlessruby.rb
@@ -65,138 +65,47 @@ module EndlessRuby
   alias PR2ER pure_ruby_to_endless_ruby
 
   private
-  def endless_ruby_to_pure_ruby_18 options
-    flg = false
-    opts = {}
+  def merge_options options
+    opts = {
+      :out => { :io => Tempfile.new("endlessruby pure temp file"), :ensure => proc { opts[:out][:io].close } }
+    }
+
     if options.respond_to? :to_hash
       opts = opts.merge options.to_hash
     elsif options.respond_to? :to_str
+      # 文字列を指定された場合はその文字列をパースする
       io = Tempfile.new "endlessruby from temp file"
       io.write options.to_str
       io.seek 0
-      opts[:io] = io
-      flg = true
+      opts[:in] = {
+        :io => io,
+        :ensure => proc { io.close }
+      }
+
     elsif options.respond_to? :to_io
-      opts[:io] = options.to_io
+      opts[:in] = {
+        :io => options.to_io,
+        :ensure => proc {}
+      }
+    else
+      fail
     end
 
-    io = opts[:io]
-    io.seek 0
-
-    r = RubyLex.new
-    r.set_input io
-    r.skip_space = false
-
-    pure = Tempfile.new "endlessruby pure temp file"
-
-    indent = []
-    pass = []
-    bol = true
-    bol_indent = 0
-
-    while t = r.token
-
-      if bol && !(RubyToken::TkSPACE === t) && !(RubyToken::TkNL === t)
-          
-        bol_indent = this_indent = t.char_no
-        while indent.last && pass.last && this_indent <= indent.last && !pass.last.include?(t.class)
-          if RubyToken::TkEND === t && this_indent == indent.last
-            indent.pop
-            pass.pop
-            next
-          end
-
-          _indent = indent.pop
-          pass.pop
-          idt = []
-          loop do
-            pure.seek pure.pos - 1
-            break if pure.pos == 0 || !(["\n", " "].include?(c = pure.read(1)))
-            pure.seek pure.pos - 1
-            idt.unshift c
-          end
-          if idt.first == "\n"
-            pure.write idt.shift
-          end
-          pure.write "#{' '*_indent}end\n"
-          pure.write idt.join
-        end
-        bol = false
-      end
-
-      case t
-      when RubyToken::TkNL
-        bol = true
-      when RubyToken::TkEND
-        indent.pop
-        pass.pop
-        next if @decompile
-      when RubyToken::TkIF, RubyToken::TkUNLESS
-        pass << [RubyToken::TkELSE, RubyToken::TkELSIF]
-        indent << t.char_no
-      when RubyToken::TkWHILE, RubyToken::TkUNTIL
-        pass << []
-        indent << t.char_no
-      when RubyToken::TkBEGIN
-        pass << [RubyToken::TkRESCUE, RubyToken::TkELSE, RubyToken::TkENSURE]
-        indent << t.char_no
-      when RubyToken::TkDEF
-        pass << [RubyToken::TkRESCUE, RubyToken::TkELSE, RubyToken::TkENSURE]
-        indent << t.char_no
-      when RubyToken::TkCLASS, RubyToken::TkMODULE
-        pass << []
-        indent << t.char_no
-      when RubyToken::TkCASE
-        pass << [RubyToken::TkWHEN, RubyToken::TkELSE]
-        indent << t.char_no
-      when RubyToken::TkDO
-        pass << []
-        indent << bol_indent
-      when RubyToken::TkSPACE
-      end
-
-      pos = io.pos
-      io.seek t.seek
-      pure.write io.read(r.seek - t.seek)
-      io.seek pos
-    end
-
-    until @decompile || (indent.empty? && pass.empty?)
-      _indent = indent.pop
-      pass.pop
-      pure.write "#{' '*_indent}end\n"
-    end
-
-    io.close if flg
-    pure.seek 0
-    pure.read.chomp
+    opts
   end
 
-  def endless_ruby_to_pure_ruby_19 options
-    flg = false
-    opts = {}
-    if options.respond_to? :to_hash
-      opts = opts.merge options.to_hash
-    elsif options.respond_to? :to_str
-      io = Tempfile.new "endlessruby from temp file"
-      # io.set_encoding 'UTF-8', 'UTF-8'
-      io.write options.to_str
-      io.seek 0
-      opts[:io] = io
-      flg = true
-    elsif options.respond_to? :to_io
-      opts[:io] = options.to_io
-    end
+  public
+  # EndlessRubyの構文をピュアなRubyの構文に変換します。
+  def endless_ruby_to_pure_ruby options
+    opts = merge_options options
 
-    io = opts[:io]
-    io.seek 0
+    io = opts[:in][:io]
 
     r = RubyLex.new
     r.set_input io
     r.skip_space = false
 
-    pure = Tempfile.new "endlessruby pure temp file"
-    # pure.set_encoding 'UTF-8', 'UTF-8'
+    pure = opts[:out][:io]
 
     indent = []
     pass = []
@@ -221,7 +130,12 @@ module EndlessRuby
           idt = []
           loop do
             pure.seek pure.pos - 1
-            break if pure.pos == 0 || !(["\n", ' '].include?(c = pure.getc))
+            if RUBY_VERSION < "1.9" # 1.8
+              c = pure.read(1)
+            else
+              c = pure.getc
+            end
+            break if pure.pos == 0 || !(["\n", ' '].include?(c))
             pure.seek pure.pos - 1
             idt.unshift c
           end
@@ -269,12 +183,23 @@ module EndlessRuby
       when RubyToken::TkSPACE
       end
 
+
       pos = io.pos
-      io.seek last[0]
-      (r.seek - last[1]).times do
-        pure.write io.getc
+      io.seek pos
+
+      pos = io.pos
+
+      if RUBY_VERSION < "1.9" # 1.8
+        io.seek t.seek
+        pure.write io.read(r.seek - t.seek)
+      else # 1.9
+        io.seek last[0]
+        (r.seek - last[1]).times do
+          pure.write io.getc
+        end
+        last = [io.pos, r.seek]
       end
-      last = [io.pos, r.seek]
+
       io.seek pos
     end
 
@@ -282,26 +207,26 @@ module EndlessRuby
       _indent = indent.pop
       pass.pop
       pure.seek pure.pos - 1
-      if pure.getc == "\n"
+      if RUBY_VERSION < "1.9" # 1.8
+        c = pure.read 1
+      else
+        c = pure.getc
+      end
+      if c  == "\n"
         pure.write "#{' '*_indent}end"
       else
         pure.write "\n#{' '*_indent}end"
       end
     end
 
-    io.close if flg
     pure.seek 0
-    pure.read.chomp
-  end
+    ret = pure.read.chomp
+    pure.seek 0
 
-  public
-  # EndlessRubyの構文をピュアなRubyの構文に変換します。
-  def endless_ruby_to_pure_ruby options
-    if RUBY_VERSION < "1.9" # 1.8
-      endless_ruby_to_pure_ruby_18 options
-    else
-      endless_ruby_to_pure_ruby_19 options
-    end
+    opts[:out][:ensure].call
+    opts[:in][:ensure].call
+
+    ret
   end
 
   alias to_pure_ruby endless_ruby_to_pure_ruby
